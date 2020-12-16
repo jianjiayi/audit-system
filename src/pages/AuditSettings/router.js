2125b7aa0b7f497ce5a17c62332f5f05c9a96fb1
Jian ShuangPeng <jianshuangpeng@people.com>
2020-12-16 14:43:52 +0800

    优化项目

--- src/pages/AuditSettings/router.js
+++ src/pages/AuditSettings/router.js
@@ -2,28 +2,28 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 const routes = () => ({
-    path: `/setting`,
-    name: '审核设置',
-    icon: 'UnorderedListOutlined',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: `/setting/`,
-        name: '审核设置',
-        component: './AuditSettings/routes/Index',
-        access: 'canAdmin',
-        hideInMenu:true,
-      },
-      {
-        path: `/setting/details/`,
-        name: '角队列详情色管理',
-        component: './AuditSettings/routes/Details',
-        access: 'canAdmin',
-        hideInMenu:true,
-      },
-      {
-        component: './404',
-      },
-    ],
-  });
+  path: `/setting`,
+  name: '审核设置',
+  icon: 'UnorderedListOutlined',
+  access: 'canAdmin',
+  routes: [
+    {
+      path: `/setting/`,
+      name: '审核设置',
+      component: './AuditSettings/routes/Index',
+      access: 'canAdmin',
+      hideInMenu: true,
+    },
+    {
+      path: `/setting/details/`,
+      name: '队列详情',
+      component: './AuditSettings/routes/Details',
+      access: 'canAdmin',
+      hideInMenu: true,
+    },
+    {
+      component: './404',
+    },
+  ],
+});
 export default routes;
