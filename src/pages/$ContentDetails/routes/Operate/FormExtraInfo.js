431e7a237e6dc304f68abcff707981e8e76b35e1
Jian ShuangPeng <jianshuangpeng@people.com>
2020-12-17 17:10:06 +0800

    优化项目

--- src/pages/$ContentDetails/routes/Operate/FormExtraInfo.js
+++ src/pages/$ContentDetails/routes/Operate/FormExtraInfo.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/dot-notation */
 import React from 'react';
 import classNames from 'classnames';
-import { connect } from 'dva';
+import { connect } from 'umi';
 
 import styles from './FormExtraInfo.module.less';
 
@@ -16,8 +16,8 @@ function FormExtraInfo(props) {
   return (
     <div className={classNames(className, styles['container'])}>
       <p>
-        <span>抓取来源ID: {curArt.sourceName || ''}</span>
-        <span>抓取来源： {curArt.sourceId || ''}</span>
+        <span>抓取来源ID: {curArt.sourceId || ''}</span>
+        <span>抓取来源： {curArt.sourceName || ''}</span>
       </p>
       <p>
         原文链接： <a href={curArt.originUrl || ''}>{curArt.originUrl || ''}</a>
